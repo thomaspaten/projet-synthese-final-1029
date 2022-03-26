c54a0859eb969636f0f4e94318b1c38001426290
thomaspaten <thomas.patenaude.poulin@gmail.com>
2022-03-26 17:45:29 -0400

    Commit Acceuil-Entreprise-Entreprise-details

---
+++ src/app/entreprises-details/entreprises-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entreprises-details',
+  templateUrl: './entreprises-details.component.html',
+  styleUrls: ['./entreprises-details.component.sass']
+})
+export class EntreprisesDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
