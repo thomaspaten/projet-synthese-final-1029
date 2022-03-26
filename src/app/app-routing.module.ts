a4c41885e95e15b4084538ce0fe145876b6919b3
thomaspaten <thomas.patenaude.poulin@gmail.com>
2022-03-26 18:08:52 -0400

    make ng routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {     AccueilComponent } from './accueil/accueil.component';
-
+import { AccueilComponent } from './accueil/accueil.component';
+import { EntreprisesComponent } from './entreprises/entreprises.component';
+import { EntreprisesDetailsComponent } from './entreprises-details/entreprises-details.component';
+import { FormAjoutCandidatComponent } from './form-ajout-candidat/form-ajout-candidat.component';
+import { ListeDesCandidatsComponent } from './liste-des-candidats/liste-des-candidats.component';
 const routes: Routes = [
-  { path: 'accueil', component: AccueilComponent }
+  { path: '', component: AccueilComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'entreprises', component: EntreprisesComponent },
+  { path: 'entreprises-details', component: EntreprisesDetailsComponent },
+  { path: 'form-ajout-candidat', component: FormAjoutCandidatComponent },
+  { path: 'liste-candidats', component: ListeDesCandidatsComponent}
 ];
 
 @NgModule({
